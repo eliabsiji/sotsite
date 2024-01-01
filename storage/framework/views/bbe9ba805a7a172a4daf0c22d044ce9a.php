f0f174d34135ee5e028f4d0c0e6a809bc91d4af3
Eliab <Eliabsiji@gmail.com>
2024-01-01 11:24:25 +0100

    working

--- storage/framework/views/bbe9ba805a7a172a4daf0c22d044ce9a.php
+++ storage/framework/views/bbe9ba805a7a172a4daf0c22d044ce9a.php
@@ -175,7 +175,7 @@
 						<div class="owl-stage">
 
 							<!-- Carousel Slide 1 -->
-							<div class="owl-item position-relative overflow-hidden" style="background-image: url(<?php echo e(asset('html/img/demos/church/slides/slide-1.jpg')); ?>); background-size: cover; background-position: center; background-position: center;">
+							<div class="owl-item position-relative overflow-hidden" style="background-image: url(<?php echo e(asset('html/img/demos/church/slides/3.jpg')); ?>); background-size: cover; background-position: center; background-position: center;">
 								<div class="container position-relative z-index-3 h-100">
 									<div class="row align-items-center justify-content-center h-100">
 										<div class="col-lg-7">
@@ -193,7 +193,25 @@
 							</div>
 
 							<!-- Carousel Slide 2 -->
-							<div class="owl-item position-relative overflow-hidden" style="background-image: url(<?php echo e(asset('html/img/demos/church/slides/slide-2.jpg')); ?>); background-size: cover; background-position: center; background-position: center;">
+							<div class="owl-item position-relative overflow-hidden" style="background-image: url(<?php echo e(asset('html/img/demos/church/slides/7.jpg')); ?>); background-size: cover; background-position: center; background-position: center;">
+								<div class="container position-relative z-index-3 h-100">
+									<div class="row align-items-center justify-content-center h-100">
+										<div class="col-lg-7">
+											<div class="d-flex flex-column justify-content-center align-items-center text-center h-100">
+												<span class="position-relative text-color-light text-6 line-height-5 font-weight-medium custom-secondary-font pe-4 mb-0 appear-animation" data-appear-animation="fadeInDownShorter" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">
+													Together
+												</span>
+												<h1 class="text-color-light font-weight-extra-bold text-13 line-height-1 mb-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="750" data-plugin-options="{'minWindowWidth': 0}">We Can Make Difference</h1>
+												<p class="text-5 text-color-light font-weight-light custom-secondary-font mb-5 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1000">JOIN OUR COMMUNITY CHURCH</p>
+												<a href="<?php echo e(asset('html/demo-church-about-us.html')); ?>" class="btn btn-primary font-weight-bold btn-py-2 btn-px-4 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1250">LEARN MORE</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+                            <!-- Carousel Slide 2 -->
+							<div class="owl-item position-relative overflow-hidden" style="background-image: url(<?php echo e(asset('html/img/demos/church/slides/5.jpg')); ?>); background-size: cover; background-position: center; background-position: center;">
 								<div class="container position-relative z-index-3 h-100">
 									<div class="row align-items-center justify-content-center h-100">
 										<div class="col-lg-7">
